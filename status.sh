30e21dcc14e33ef77a2dfcad64ce67eb54a5044a
Adrian <relex12@free.fr>
2024-06-24 19:00:54 +0200

    status.sh: add git remote update (silenced) and git push quiet

--- status.sh
+++ status.sh
@@ -10,9 +10,12 @@ popd () {
 cd ..
 for dir in `ls`
   do pushd $dir
+  git remote update > /dev/null
   if [[ ! -z $(git status --short) ]] ; then
     echo $dir
     git status
+  else
+    git push --quiet
   fi
   popd
 done
