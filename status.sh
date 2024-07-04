a2df44ca1496bcf815bb5cd137ac5fb316314199
Adrian Bonnet <adrian.2.bonnet.external@eviden.com>
2024-07-04 11:51:51 +0200

    status.sh: use shorten status and add pull before push

--- status.sh
+++ status.sh
@@ -10,11 +10,13 @@ popd () {
 cd ..
 for dir in `ls`
   do pushd $dir
+  # echo $dir
   git remote update > /dev/null
   if [[ ! -z $(git status --short) ]] ; then
     echo $dir
-    git status
+    git status --short
   else
+    git pull --quiet
     git push --quiet
   fi
   popd
